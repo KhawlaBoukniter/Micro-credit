04410a76b0b2c4c07b70bd164cf6b3fe76120fcf
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-10-04 16:31:00 +0100

    ajout des menus et du point d'entrée Main pour l'application micro-crédit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Views.MenuPrincipal;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
+        Scanner sc = new Scanner(System.in);
+        MenuPrincipal menuPrincipal = new MenuPrincipal(sc);
+        menuPrincipal.afficherMenuPrincipal();
     }
 }
